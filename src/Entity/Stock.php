69cdc19d24f5d2bd60354b3e198f1f98ea2ff031
tareq-salem <38789773+tareq-salem@users.noreply.github.com>
2019-04-09 12:00:41 +0200

    creation enities Stock et Role (#1)

---
+++ src/Entity/Stock.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\StockRepository")
+ */
+class Stock
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nb_product;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNbProduct(): ?int
+    {
+        return $this->nb_product;
+    }
+
+    public function setNbProduct(int $nb_product): self
+    {
+        $this->nb_product = $nb_product;
+
+        return $this;
+    }
+}
